141c90536ca92bdaf2786883320396ff00122ef0
Mike Urbanski <mikeurbanski1@gmail.com>
2024-11-01 21:24:57 -0500

    add utils tests

---
+++ packages/utils/test/index.test.ts
@@ -0,0 +1,16 @@
+import { expect, describe, it } from 'vitest';
+import { mapToRecord } from '../src';
+
+describe('utils tests', () => {
+    it('should convert a map to a record', () => {
+        expect(
+            mapToRecord(
+                new Map([
+                    ['a', 1],
+                    ['b', 2]
+                ])
+            )
+        ).toEqual({ a: 1, b: 2 });
+    });
+});
+
